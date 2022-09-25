e41b43e4a3943a76fb5baac6b4dc37b6c08b41f9
Luzarait1 <111089098+Luzarait1@users.noreply.github.com>
2022-09-24 20:53:40 -0500

    carpetas

---
+++ src/main/java/zooAnimales/Pez.java
@@ -0,0 +1,42 @@
+package zooAnimales;
+import java.util.ArrayList;
+
+import gestion.Zona;
+
+public class Pez extends Animal{
+    private ArrayList<Pez> listado;
+    public int salmones;
+    public int bacalaos;
+    private String colorEscamas;
+    private int cantidadAletas;
+    
+    public Pez() {
+
+    }  
+    
+    public Pez(int totalAnimales, String nombre, int edad, String habitat, String genero, ArrayList<Zona> zona,
+            ArrayList<Pez> listado, int salmones, int bacalaos, String colorEscamas, int cantidadAletas) {
+        super(totalAnimales, nombre, edad, habitat, genero, zona);
+        this.listado = listado;
+        this.salmones = salmones;
+        this.bacalaos = bacalaos;
+        this.colorEscamas = colorEscamas;
+        this.cantidadAletas = cantidadAletas;
+    }
+
+    public void crearSalmon(int salmones) {
+        this.salmones = salmones;
+        new Pez(totalAnimales, nombre, edad, "oceano", genero, zona, listado, salmones, bacalaos, "rojo", 6);
+    }
+
+    public void crearBacalao(int bacalaos) {
+        this.bacalaos = bacalaos;
+        new Pez(totalAnimales, nombre, edad, "oceano", genero, zona, listado, salmones, bacalaos, "gris", 6);
+    }
+
+    public int cantidadPeces() {
+        return listado.size(); 
+    }
+
+    
+}
---
+++ src/main/java/zooAnimales/Reptil.java
@@ -0,0 +1,43 @@
+package zooAnimales;
+
+import java.util.ArrayList;
+
+import gestion.Zona;
+
+public class Reptil extends Animal{
+    private ArrayList <Reptil> listado;
+    public int iguanas;
+    public int serpientes;
+    private String colorEscamas;
+    private int largoCola;
+
+    public Reptil() {
+
+    }  
+    
+    public Reptil(int totalAnimales, String nombre, int edad, String habitat, String genero, ArrayList<Zona> zona,
+            ArrayList<Reptil> listado, int iguanas, int serpientes, String colorEscamas, int largoCola) {
+        super(totalAnimales, nombre, edad, habitat, genero, zona);
+        this.listado = listado;
+        this.iguanas = iguanas;
+        this.serpientes = serpientes;
+        this.colorEscamas = colorEscamas;
+        this.largoCola = largoCola;
+    }
+
+    public void setIguana(int iguanas) {
+        this.iguanas = iguanas;
+        new Reptil(totalAnimales, nombre, edad, "humedal", genero, zona, listado, iguanas, serpientes, "verde", 3);
+    }
+
+    public void crearSerpiente(int serpientes) {
+        this.serpientes = serpientes;
+        new Reptil(totalAnimales, nombre, edad, "jungla", genero, zona, listado, iguanas, serpientes, "blanco", 1);
+    }
+    
+    public int cantidadReptiles() {
+        return listado.size(); 
+    }
+
+    
+}
